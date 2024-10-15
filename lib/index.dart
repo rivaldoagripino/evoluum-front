0b462f94a79def74550600f4c27d80017d99e3d3
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-10-15 16:28:55 +0000

    Adjust modules code

--- lib/index.dart
+++ lib/index.dart
@@ -1,3 +1,3 @@
 // Export pages
-export '/modules/login/login_page/login_page_widget.dart' show LoginPageWidget;
-export '/modules/home/home_page/home_page_widget.dart' show HomePageWidget;
+export '/modules/login_page/login_page_widget.dart' show LoginPageWidget;
+export '/modules/home_page/home_page_widget.dart' show HomePageWidget;
